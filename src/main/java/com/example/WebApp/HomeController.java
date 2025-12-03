a3a944d5c4b7fceec4603383e8c4957c061fa468
Izabelle Tais <izabelle_tais@outlook.com>
2025-12-03 08:09:10 -0300

    Consertando o build (Passo 6)

--- src/main/java/com/example/WebApp/HomeController.java
+++ src/main/java/com/example/WebApp/HomeController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class HomeController {
 	@RequestMapping("/")
 	public @ResponseBody String greeting() {
-		return "Hello, Docker!";
+		return "Hello, World!";
 	}
 	
 	@RequestMapping("/izabelle")
